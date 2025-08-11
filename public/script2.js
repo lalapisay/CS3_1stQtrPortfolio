693946e931851536271648016b2ba3b548d9d5c1
lalapisay <b2029laepauig@pshs.edu.ph>
2025-08-11 12:07:48 +0800

    I'll still edit this commit

---
+++ public/script2.js
@@ -0,0 +1,11 @@
+let Age;
+
+let maxAge;
+
+let numPerDay;
+
+let totalRequired;
+totalRequired =  (numPerDay * 365) * (maxAge - Age);
+
+let answer = "You'll need " + totalRequired + " chocolates to last you until the age of " + maxAge;
+document.write(answer);
